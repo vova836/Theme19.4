2f38c9cbc2b49bd9665bc2664bc029374d6f58ba
vova836 <123488703+vova836@users.noreply.github.com>
2024-04-19 14:21:30 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TourOperator> agencies = new List<TourOperator>
+        {
+            new TourOperator("Агентство1", 2015, "123456789", 100000),
+            new TourOperator("Агентство2", 2010, "987654321", 150000),
+            new TourOperator("Агентство3", 1995, "456789123", 120000)
+        };
+
+            decimal totalProfit = agencies.Sum(a => a.Profit);
+            Console.WriteLine($"Суммарная прибыль всех агентств: {totalProfit}");
+
+            TourOperator maxProfitAgency = agencies.OrderByDescending(a => a.Profit).First();
+            Console.WriteLine($"Агентство с самой высокой прибылью: {maxProfitAgency.Name} с прибылью { maxProfitAgency.Profit}");
+
+            string bankruptAgencyINN = "987654321"; //  ИНН агентства-банкрота
+            var bankruptAgency = agencies.FirstOrDefault(a => a.INN == bankruptAgencyINN);
+            if (bankruptAgency != null)
+            {
+                agencies.Remove(bankruptAgency);
+                Console.WriteLine($"Банкротное агентство {bankruptAgency.Name} удален из списка.");
+            }
+
+            TourOperator newAgency = new TourOperator("Новое Агенство", 2021, "654987321", 80000);
+            agencies.Add(newAgency);
+
+            agencies = agencies.OrderBy(a => a.Profit).ToList();
+            Console.WriteLine("Список агентств, отсортированный по прибыли:");
+            foreach (var agency in agencies)
+            {
+                agency.GetInfo();
+            }
+            Console.ReadKey();
+        }      
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/TourOperator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Создаем класс Туристическая фирма
+    /// </summary>
+    class TourOperator
+    {  
+        /// <summary>
+        /// Создаем свойства поля классу Туристическая фирма
+        /// </summary>
+        public string Name { get; set; }
+        public int YearEstablished { get; set; }
+        public string INN { get; set; }
+        public decimal Profit { get; set; }
+
+        /// <summary>
+        /// Создаем методы класса Туристическая фирма
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="yearEstablished"></param>
+        /// <param name="inn"></param>
+        /// <param name="profit"></param>
+        public TourOperator(string name, int yearEstablished, string inn, decimal profit)
+        {
+            Name = name;
+            YearEstablished = yearEstablished;
+            INN = inn;
+            Profit = profit;
+        }
+
+        /// <summary>
+        /// Создваем метод вывода о информации из класса Туристическая фирма
+        /// </summary>
+        public void GetInfo()
+        {
+            Console.WriteLine($"Туристическое агентство: {Name}\nГод создания: {YearEstablished}\nИИН: {INN}\nПрибыль за текущий год: {Profit}\n");
+        }
+    }
+
+}
